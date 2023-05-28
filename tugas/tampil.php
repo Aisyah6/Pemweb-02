e422dd7551b171d1bfb77ed78b1706542f48c769
Aisyah6 <114473036+Aisyah6@users.noreply.github.com>
2023-05-28 17:56:48 +0700

    Add files via upload

---
+++ tugas/tampil.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  th, td {
+    border: 1px solid black;
+    padding: 8px;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+</style>
+</head>
+<body>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Nama</th>
+        <th scope="col">Tanggal pemeriksaan</th>
+        <th scope="col">Tanggal lahir/usia</th>
+        <th scope="col">Jenis kelamin</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Shiddiq</td>
+        <td>20 Mei 2023</td>
+        <td>20 September 2003/ 19 tahun</td>
+        <td>Laki - laki</td>
+      </tr>
+      <tr>
+        <td>Aisyah</td>
+        <td>19 Mei 2023</td>
+        <td>16 April 2001/ 21 tahun</td>
+        <td>Perempuan</td>
+      </tr>
+      <tr>
+        <td>Nazril</td>
+        <td>12 Mei 2023</td>
+        <td>20 Juli 2015/ 8 tahun</td>
+        <td>Laki - laki</td>
+      </tr>
+      <tr>
+        <td>Suci</td>
+        <td>23 Mei 2023</td>
+        <td>31 Juli 2005/ 17 tahun</td>
+        <td>Perempuan</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
